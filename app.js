c1f4b1230630af5924f78af41196a1a60fd70c5a
Asha Bijarnia <asha.bijarnia@cygnetinfotech.com>
2018-12-07 16:45:57 +0530

    resolve conflicts.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 console.log('hello');
 
 console.log('changes in master branch');
+console.log('changes in login func');
+
 console.log('commit for login screen');
 
 console.log('commit for registration screen');
